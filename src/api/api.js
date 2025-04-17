68da1bb357517e261e33e76f5cde6879898d6ab0
Edson Jengwa <edson.jengwa@gmail.com>
2025-04-17 12:34:32 +0200

    Added environment variables

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: '',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
 export const getAllCountries = () => api.get('/countries')
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src', // This matches the Jest alias
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/countries': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
+    server: {
+      proxy: {
+        '/countries': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        }
+      } 
     }
   }
 })
-
-
